224d5f2449cbad6dfd53e746b1f07b13fd606178
Joel Hellberg <JoelHellberg@users.noreply.github.com>
2025-05-08 09:26:34 +0200

    Funkar & Uppstyckad

--- Lab3/WumpusWorld/MyAgent.java
+++ Lab3/WumpusWorld/MyAgent.java
@@ -1,354 +1,190 @@
 import java.util.*;
 
-public class MyAgent implements Agent
-{
+public class MyAgent implements Agent {
     private World w;
+    Random rand = new Random();
     List<String> visitedPositions = new ArrayList<>();
     Set<Clause> KB = new LinkedHashSet<>();
-    
-    public MyAgent(World world)
-    {
-        for (int i = 1; i <=4; i++) {
-            for (int j = 1; j <=4; j++) {
-                String position = "(" + i + ","  + j +")";
-                Clause c = new Clause(Set.of(position, "pit", "wumpus"));
-                KB.add(c);
-            }
-        }
+
+    public MyAgent(World world) {
         w = world;
     }
 
- 
-    public void shootWumpusIfPossible(String possibleWumpusPos) {
-        int cX = w.getPlayerX();
-        int cY = w.getPlayerY();
-        String currentPos = "(" + cX + "," + cY + ")";
-    
-        // Only proceed if this is a new position
-        if (!Arrays.asList(visitedPositions).contains(currentPos)) {
-            List<String> safePositions = new ArrayList<>(List.of("(1,1)"));
-    
-            // Resolve known facts
-            KB = CNFResolver.runResolution(KB);
-    
-            for (Clause clause : KB) {
-                if (clause.literals.size() == 1) {
-                    safePositions.add(clause.literals.iterator().next());
-                }
-            }
-    
-            // Try to deduce Wumpus position
-            Set<Clause> testKB = new LinkedHashSet<>(KB);
-            Clause wumpusHint = new Clause(Set.of(possibleWumpusPos, "~wumpus"));
-            testKB.add(wumpusHint);
-            testKB = CNFResolver.runResolution(testKB);
-    
-            List<String> wumpusCandidates = new ArrayList<>();
-            for (Clause clause : testKB) {
-                if (clause.literals.size() == 1) {
-                    wumpusCandidates.add(clause.literals.iterator().next());
-                }
-            }
-    
-            // Filter out known safe positions
-            wumpusCandidates.removeAll(safePositions);
-    
-            if (!wumpusCandidates.isEmpty()) {
-                String candidate = wumpusCandidates.get(0);
-                System.out.println("Wumpus candidate: " + candidate);
-    
-                String[] parts = candidate.replaceAll("[()]", "").split(",");
-                int targetX = Integer.parseInt(parts[0]);
-                int targetY = Integer.parseInt(parts[1]);
-    
-                // Check adjacency
-                if (targetX == cX && targetY == cY + 1) {
-                    System.out.println("SHOOT UP");
-                    w.doAction(World.A_SHOOT_UP);
-                } else if (targetX == cX && targetY == cY - 1) {
-                    System.out.println("SHOOT DOWN");
-                    w.doAction(World.A_SHOOT_DOWN);
-                } else if (targetX == cX - 1 && targetY == cY) {
-                    System.out.println("SHOOT LEFT");
-                    w.doAction(World.A_SHOOT_LEFT);
-                } else if (targetX == cX + 1 && targetY == cY) {
-                    System.out.println("SHOOT RIGHT");
-                    w.doAction(World.A_SHOOT_RIGHT);
-                } else {
-                    System.out.println("Shooting target is not adjacent!");
-                }
-    
-                if (!w.wumpusAlive()) {
-                    KB.add(new Clause(Set.of("~wumpus")));
-                }
-            }
-        }
+    private List<String> getAdjacentPositions(int x, int y) {
+        List<String> positions = new ArrayList<>();
+        if (x > 1) positions.add("(" + (x - 1) + "," + y + ")");
+        if (x < 4) positions.add("(" + (x + 1) + "," + y + ")");
+        if (y > 1) positions.add("(" + x + "," + (y - 1) + ")");
+        if (y < 4) positions.add("(" + x + "," + (y + 1) + ")");
+        return positions;
     }
     
+    private void handlePercept(String pos, String fact, boolean negate) {
+        if (negate) fact = "~" + fact;
+        KB.add(new Clause(Set.of(pos, fact)));
+    }
 
-// Ask your solver to do an action
-
-    public void doAction()
-    {
-        //Location of the player
-        int cX = w.getPlayerX();
-        int cY = w.getPlayerY();
-        System.out.println("Current position: (" + cX + ", " + cY + ")");
-        
-        //Basic action:
-        //Grab Gold if we can.
-        if (w.hasGlitter(cX, cY))
-        {
-            w.doAction(World.A_GRAB);
-            return;
-        }
-        
-        //Basic action:
-        //We are in a pit. Climb up.
-        if (w.isInPit())
-        {
-            w.doAction(World.A_CLIMB);
-            return;
-        }
-
-        
-        
-        //Test the environment
-        if (w.hasBreeze(cX, cY) && w.hasStench(cX, cY))
-        {
-            System.out.println("I am in a Breeze and Stench");
-            if(cX > 1) {
-                String neighbourPos = "(" + (cX - 1) + "," + cY +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cX < 4) {
-                String neighbourPos = "(" + (cX + 1) + "," + cY +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cY > 1) {
-                String neighbourPos = "(" + (cX) + "," + (cY - 1) +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cY < 4) {
-                String neighbourPos = "(" + (cX) + "," + (cY + 1) +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-        }
-        
-        if (w.hasBreeze(cX, cY))
-        {
-            if(cX > 1) {
-                Clause c = new Clause(Set.of("(" + (cX - 1) + "," + cY +  ")", "pit"));
-                KB.add(c);
-            }
-            if(cX < 4) {
-                Clause c = new Clause(Set.of("(" + (cX + 1) + "," + cY +  ")", "pit"));
-                KB.add(c);
-            }
-            if(cY > 1) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY - 1) +  ")", "pit"));
-                KB.add(c);
-            }
-            if(cY < 4) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY + 1) +  ")", "pit"));
-                KB.add(c);
-            }
-
-            System.out.println("I am in a Breeze");
-        }
-        if (!(w.hasBreeze(cX, cY)))
-        {
-            if(cX > 1) {
-                Clause c = new Clause(Set.of("(" + (cX - 1) + "," + cY +  ")", "~pit"));
-                KB.add(c);
-            }
-            if(cX < 4) {
-                Clause c = new Clause(Set.of("(" + (cX + 1) + "," + cY +  ")", "~pit"));
-                KB.add(c);
-            }
-            if(cY > 1) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY - 1) +  ")", "~pit"));
-                KB.add(c);
-            }
-            if(cY < 4) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY + 1) +  ")", "~pit"));
-                KB.add(c);
-            }
-
-            System.out.println("I am not in a Breeze");
-        }
-        if (w.hasStench(cX, cY))
-        {
-            if(cX > 1) {
-                String neighbourPos = "(" + (cX - 1) + "," + cY +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cX < 4) {
-                String neighbourPos = "(" + (cX + 1) + "," + cY +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cY > 1) {
-                String neighbourPos = "(" + (cX) + "," + (cY - 1) +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            if(cY < 4) {
-                String neighbourPos = "(" + (cX) + "," + (cY + 1) +  ")";
-                shootWumpusIfPossible(neighbourPos);
-                Clause c = new Clause(Set.of(neighbourPos, "pit", "wumpus"));
-                KB.add(c);
-            }
-            System.out.println("I am in a Stench");
-        }
-        if (!(w.hasStench(cX, cY)))
-        {
-            if(cX > 1) {
-                Clause c = new Clause(Set.of("(" + (cX - 1) + "," + cY +  ")", "~wumpus"));
-                KB.add(c);
-            }
-            if(cX < 4) {
-                Clause c = new Clause(Set.of("(" + (cX + 1) + "," + cY +  ")", "~wumpus"));
-                KB.add(c);
-            }
-            if(cY > 1) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY - 1) +  ")", "~wumpus"));
-                KB.add(c);
-            }
-            if(cY < 4) {
-                Clause c = new Clause(Set.of("(" + (cX) + "," + (cY + 1) +  ")", "~wumpus"));
-                KB.add(c);
-            }
-            System.out.println("I am not in a Stench");
-        }
-        if (w.hasPit(cX, cY))
-        {
-            System.out.println("I am in a Pit");
-        }
-
-        visitedPositions.add("(" + cX + "," + cY + ")");
+    private List<String> findSafePositions() {
         KB = CNFResolver.runResolution(KB);
-
-         
-        for (Clause c : KB) {
-            System.out.println(c);
-        }
-
-        System.out.println("\nConclusion:");
-        List<String> safePositions = new ArrayList<>();
-        safePositions.add("(1,1)");
-        for (Clause c : KB) {
-            if (c.literals.size() == 1) {
-                safePositions.add(c.literals.iterator().next());
-                System.out.println(" - " + c.literals.iterator().next());
+        List<String> safePositions = new ArrayList<>(List.of("(1,1)"));
+        for (Clause clause : KB) {
+            if (clause.literals.size() == 1) {
+                safePositions.add(clause.literals.iterator().next());
             }
         }
+        return safePositions;
+    }
+    
+    public void doAction() {
+        int cX = w.getPlayerX(), cY = w.getPlayerY();
+        String currentPos = "(" + cX + "," + cY + ")";
+        System.out.println("Current position: " + currentPos);
+    
+        if (w.hasGlitter(cX, cY)) { w.doAction(World.A_GRAB); return; }
+        if (w.isInPit()) { w.doAction(World.A_CLIMB); return; }
+    
+        boolean breeze = w.hasBreeze(cX, cY);
+        boolean stench = w.hasStench(cX, cY);
+        List<String> neighbors = getAdjacentPositions(cX, cY);
+    
+        if (breeze && stench) {
+            System.out.println("I am in a Breeze and Stench");
+            for (String pos : neighbors) {
+                shootWumpusIfPossible(pos, currentPos);
+                KB.add(new Clause(Set.of(pos, "pit", "wumpus")));
+            }
+        } else if (!breeze && !stench) {
+            System.out.println("I am in a clear area");
+            for (String pos : neighbors) {
+                KB.add(new Clause(Set.of(pos)));
+            }
+        } else {
+            if (breeze) {
+                System.out.println("I am in a Breeze");
+                for (String pos : neighbors) handlePercept(pos, "pit", false);
+            } else {
+                System.out.println("I am not in a Breeze");
+                for (String pos : neighbors) handlePercept(pos, "pit", true);
+            }
+    
+            if (stench) {
+                System.out.println("I am in a Stench");
+                for (String pos : neighbors) {
+                    shootWumpusIfPossible(pos, currentPos);
+                    handlePercept(pos, "wumpus", false);
+                }
+            } else {
+                System.out.println("I am not in a Stench");
+                for (String pos : neighbors) handlePercept(pos, "wumpus", true);
+            }
+        }
+    
+        visitedPositions.add(currentPos);
+        KB = CNFResolver.runResolution(KB);
+    
+        List<String> safePositions = findSafePositions();
 
-        List<String> possibleMoves = new ArrayList<>();
+        List<String> adjacentSafeMoves = new ArrayList<>(getAdjacentPositions(cX, cY));
+        adjacentSafeMoves.retainAll(safePositions);
 
-        if(cX > 1) {
-            possibleMoves.add("(" + (cX - 1) + "," + cY +  ")");
-        }
-        if(cX < 4) {
-            possibleMoves.add("(" + (cX + 1) + "," + cY +  ")");
-        }
-        if(cY > 1) {
-            possibleMoves.add("(" + cX + "," + (cY - 1) +  ")");
-        }
-        if(cY < 4) {
-            possibleMoves.add("(" + cX + "," + (cY + 1) +  ")");
-        }
-
-        List<String> safePossibleMoves = new ArrayList<>(safePositions);
-        safePossibleMoves.retainAll(possibleMoves);
-        String bestMove = "";
-
-        List<String> unvisitedSafeMoves = new ArrayList<>(safePossibleMoves);
-        unvisitedSafeMoves.removeAll(visitedPositions);
-
-        for(String move : visitedPositions) {
-            System.out.println("Visited pos: " + move);
-        }
-
-        for(String move : unvisitedSafeMoves) {
-            System.out.println("unvisited safe move: " + move);
-        }
-
-        if(unvisitedSafeMoves.size() > 0) {
-            bestMove = unvisitedSafeMoves.get(0);
-        }
-        else {
-            bestMove = safePossibleMoves.get(0);
-        }
-        
+        List<String> unvisitedAdjacentSafeMoves = new ArrayList<>(adjacentSafeMoves);
+        unvisitedAdjacentSafeMoves.removeAll(visitedPositions);
+    
+        String bestMove = unvisitedAdjacentSafeMoves.isEmpty() 
+        ? adjacentSafeMoves.get(rand.nextInt(adjacentSafeMoves.size())) 
+        : unvisitedAdjacentSafeMoves.get(0);
 
         String[] parts = bestMove.replaceAll("[()]", "").split(",");
+        int targetX = Integer.parseInt(parts[0]), targetY = Integer.parseInt(parts[1]);
+    
+        if (targetX == cX && targetY == cY + 1) w.doAction(World.A_MOVE_UP);
+        else if (targetX == cX && targetY == cY - 1) w.doAction(World.A_MOVE_DOWN);
+        else if (targetX == cX - 1 && targetY == cY) w.doAction(World.A_MOVE_LEFT);
+        else if (targetX == cX + 1 && targetY == cY) w.doAction(World.A_MOVE_RIGHT);
+        else System.out.println("Target is not adjacent!");
+    }
 
-        // Parse integers
+    public void shootWumpusIfPossible(String possibleWumpusPos, String currentPos) {
+        if (visitedPositions.contains(currentPos)) return;
+    
+        List<String> safePositions = findSafePositions();
+        List<String> wumpusCandidates = findWumpusCandidates(possibleWumpusPos, safePositions);
+    
+        if (!wumpusCandidates.isEmpty()) {
+            handleWumpusShot(wumpusCandidates.get(0));
+        }
+    }
+    
+    private List<String> findWumpusCandidates(String possibleWumpusPos, List<String> safePositions) {
+        Set<Clause> testKB = new LinkedHashSet<>(KB);
+        testKB.add(new Clause(Set.of(possibleWumpusPos, "~wumpus")));
+        testKB = CNFResolver.runResolution(testKB);
+    
+        List<String> candidates = new ArrayList<>();
+        for (Clause clause : testKB) {
+            if (clause.literals.size() == 1) {
+                candidates.add(clause.literals.iterator().next());
+            }
+        }
+        candidates.removeAll(safePositions);
+        return candidates;
+    }
+    
+    private void handleWumpusShot(String candidate) {
+        System.out.println("Wumpus candidate: " + candidate);
+    
+        String[] parts = candidate.replaceAll("[()]", "").split(",");
         int targetX = Integer.parseInt(parts[0]);
         int targetY = Integer.parseInt(parts[1]);
-
-        // Decide movement direction
+    
+        int cX = w.getPlayerX();
+        int cY = w.getPlayerY();
+    
         if (targetX == cX && targetY == cY + 1) {
-            System.out.println("Move UP");
-            w.doAction(World.A_MOVE_UP);
+            System.out.println("SHOOT UP");
+            w.doAction(World.A_SHOOT_UP);
         } else if (targetX == cX && targetY == cY - 1) {
-            System.out.println("Move DOWN");
-            w.doAction(World.A_MOVE_DOWN);
+            System.out.println("SHOOT DOWN");
+            w.doAction(World.A_SHOOT_DOWN);
         } else if (targetX == cX - 1 && targetY == cY) {
-            System.out.println("Move LEFT");
-            w.doAction(World.A_MOVE_LEFT);
+            System.out.println("SHOOT LEFT");
+            w.doAction(World.A_SHOOT_LEFT);
         } else if (targetX == cX + 1 && targetY == cY) {
-            System.out.println("Move RIGHT");
-            w.doAction(World.A_MOVE_RIGHT);
+            System.out.println("SHOOT RIGHT");
+            w.doAction(World.A_SHOOT_RIGHT);
         } else {
-            System.out.println("Target is not adjacent!");
-}
-        
-        //Random move actions
-        //w.doAction(World.A_MOVE_UP);
-
+            System.out.println("Shooting target is not adjacent!");
+        }
+    
+        if (!w.wumpusAlive()) {
+            KB.add(new Clause(Set.of("~wumpus")));
+        }
     }
+    
 }
 
+// ACTIONS AVAILABLE
+// --------------------------------
+// w.doAction(World.A_MOVE_RIGHT);
+// w.doAction(World.A_MOVE_LEFT);
+// w.doAction(World.A_MOVE_UP);
+// w.doAction(World.A_MOVE_DOWN);
 
-            // ACTIONS AVAILABLE
-			// --------------------------------
-            // w.doAction(World.A_MOVE_RIGHT);
-            // w.doAction(World.A_MOVE_LEFT);
-            // w.doAction(World.A_MOVE_UP);
-            // w.doAction(World.A_MOVE_DOWN);
-			
-			// w.doAction(World.A_SHOOT_UP);
-			// w.doAction(World.A_SHOOT_DOWN);
-			// w.doAction(World.A_SHOOT_LEFT);
-			// w.doAction(World.A_SHOOT_RIGHT);
-			
-			// w.doAction(World.A_GRAB);
-			// w.doAction(World.A_CLIMB)
-						
-			
-			// SENSING ACTIONS (return true/false)
-			// ------------------------------------
-			// w.hasGlitter(cX,cY)
-			// w.hasBreeze(cX,cY)
-			// w.hasStench(cX, cY)
-			// w.hasPit(cX, cY)
-			// w.hasWumpus(cX, cY)
-			// w.isInPit()
-			// w.wumpusAlive()
-			// w.hasArrow()
-			
-			
+// w.doAction(World.A_SHOOT_UP);
+// w.doAction(World.A_SHOOT_DOWN);
+// w.doAction(World.A_SHOOT_LEFT);
+// w.doAction(World.A_SHOOT_RIGHT);
+
+// w.doAction(World.A_GRAB);
+// w.doAction(World.A_CLIMB)
+
+// SENSING ACTIONS (return true/false)
+// ------------------------------------
+// w.hasGlitter(cX,cY)
+// w.hasBreeze(cX,cY)
+// w.hasStench(cX, cY)
+// w.hasPit(cX, cY)
+// w.hasWumpus(cX, cY)
+// w.isInPit()
+// w.wumpusAlive()
+// w.hasArrow()
